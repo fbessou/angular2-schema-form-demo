6419023a13e69865fae5be95e318f6bfb2b055b9
Frank Bessou <fbessou@free.fr>
2016-07-13 09:14:51 +0200

    Removed unused members in App.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,23 @@
-import {Component, ComponentResolver, Directive, ElementRef, Renderer, ViewEncapsulation,ViewContainerRef,ComponentMetadata} from "@angular/core";
+import {Component, Directive } from "@angular/core";
 import {NgModel, CORE_DIRECTIVES, FORM_DIRECTIVES} from "@angular/common";
 import {Form, FieldRegistryService} from "angular2-schema-form";
 
 require("style!../bootstrap.min.css");
+require("style!./app.css");
 
 @Component({
 	selector: "schema-form-demo-app",
 	directives: [
 		Form
 	],
-	providers: [FieldRegistryService, NgModel],
+	providers: [FieldRegistryService],
 	template: require("./app.component.html"),
-	styles: [require("./app.css")],
-	encapsulation: ViewEncapsulation.None
 })
-
 export class DemoApp {
 	private schema:any;
 	private model:any;
-	private container: ViewContainerRef;
-	private resolver: ComponentResolver;
-	constructor(container: ViewContainerRef = null, resolver: ComponentResolver = null,registry: FieldRegistryService) {
-		this.container = container;
-		this.resolver = resolver;
 
+	constructor(registry: FieldRegistryService) {
 		this.schema = require("./sampleschema.json")
 		this.model = require("./samplemodel.json");
 	}
